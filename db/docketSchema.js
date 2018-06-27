66e9403f45ea8a032153caf5a6b74284eb60f351
Shriman Nijagun <EVOLVUS\shrimank@ELPBLR-025.evolvus.com>
2018-06-27 14:16:33 +0530

    eventCode property added in docketSchema.

--- db/docketSchema.js
+++ db/docketSchema.js
@@ -2,68 +2,71 @@ const mongoose = require('mongoose');
 var validate = require('mongoose-validator');
 
 var Validator = [
-  validate({
-    validator: 'isLength',
-    arguments: [3, 50],
-    message: 'Path {PATH} should be between {ARGS[0]} and {ARGS[1]} characters',
-  }),
-  validate({
-    validator: 'isAlphanumeric',
-    passIfEmpty: true,
-    message: 'Path {PATH} should contain alpha-numeric characters only',
-  }),
+    validate({
+        validator: 'isLength',
+        arguments: [3, 50],
+        message: 'Path {PATH} should be between {ARGS[0]} and {ARGS[1]} characters',
+    }),
+    validate({
+        validator: 'isAlphanumeric',
+        passIfEmpty: true,
+        message: 'Path {PATH} should contain alpha-numeric characters only',
+    })
 ];
 
 var docketSchema = new mongoose.Schema({
-  createdBy: {
-    type: String,
-    required: true,
-    validate: Validator
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  application: {
-    type: String,
-    required: true
-  },
-  source: {
-    type: String,
-    required: true,
-    validate: Validator
-  },
-  ipAddress: {
-    type: String,
-    required: true
-  },
-  level: {
-    type: String,
-    required: false
-  },
-  status: {
-    type: String,
-    required: true,
-    enum: ["SUCCESS", "FAILURE", "success", "failure"]
-  },
-  eventDateTime: {
-    type: Date,
-    required: true
-  },
-  details: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 250
-  },
-  keyDataAsJSON: {
-    type: String,
-    required: true
-  },
-  keywords: {
-    type: String,
-    required: false
-  }
+    createdBy: {
+        type: String,
+        required: true,
+        validate: Validator
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    eventCode: {
+        type: String
+    },
+    application: {
+        type: String,
+        required: true
+    },
+    source: {
+        type: String,
+        required: true,
+        validate: Validator
+    },
+    ipAddress: {
+        type: String,
+        required: true
+    },
+    level: {
+        type: String,
+        required: false
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ["SUCCESS", "FAILURE", "success", "failure"]
+    },
+    eventDateTime: {
+        type: Date,
+        required: true
+    },
+    details: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 250
+    },
+    keyDataAsJSON: {
+        type: String,
+        required: true
+    },
+    keywords: {
+        type: String,
+        required: false
+    }
 });
 
 
--- model/docketSchema.js
+++ model/docketSchema.js
@@ -2,42 +2,45 @@
  ** JSON Schema representation of the Docket model
  */
 module.exports.schema = {
-  "$schema": "http://json-schema.org/draft-06/schema#",
-  "title": "DocketModel",
-  "type": "object",
-  "properties": {
-    "name": {
-      "type": "string"
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "title": "DocketModel",
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string"
+        },
+        "eventCode": {
+            "type": "string"
+        },
+        "application": {
+            "type": "string"
+        },
+        "source": {
+            "type": "string"
+        },
+        "createdBy": {
+            "type": "string"
+        },
+        "ipAddress": {
+            "type": "string"
+        },
+        "level": {
+            "type": ["string", "null"]
+        },
+        "status": {
+            "type": "string"
+        },
+        "eventDateTime": {
+            "type": "string",
+            "format": "date-time"
+        },
+        "keyDataAsJSON": {
+            "type": "string"
+        },
+        "details": {
+            "type": "string"
+        }
     },
-    "application": {
-      "type": "string"
-    },
-    "source": {
-      "type": "string"
-    },
-    "createdBy": {
-      "type": "string"
-    },
-    "ipAddress": {
-      "type": "string"
-    },
-    "level": {
-      "type": ["string", "null"]
-    },
-    "status": {
-      "type": "string"
-    },
-    "eventDateTime": {
-      "type": "string",
-      "format": "date-time"
-    },
-    "keyDataAsJSON": {
-      "type": "string"
-    },
-    "details": {
-      "type": "string"
-    }
-  },
-  "required": ["name", "application", "source", "createdBy", "ipAddress", "status", "keyDataAsJSON", "details", "eventDateTime"]
+    "required": ["name", "application", "source", "createdBy", "ipAddress", "status", "keyDataAsJSON", "details", "eventDateTime"]
 
 };
